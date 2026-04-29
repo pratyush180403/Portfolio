9553bbfc1513589e8600aea49dd396e01760fe09
pratyush180403 <dubeypratyush9999@gmail.com>
2026-04-30 02:03:31 +0530

    website

---
+++ src/config/Footer.tsx
@@ -0,0 +1,5 @@
+export const footerConfig = {
+  developer: 'Pratyush',
+  text: 'Design & Developed by',
+  copyright: 'All rights reserved.',
+};
---
+++ src/config/Navbar.tsx
@@ -0,0 +1,19 @@
+export interface NavItem {
+  label: string;
+  href: string;
+}
+
+export const navbarConfig = {
+  logo: {
+    src: '/assets/logo.png',
+    alt: 'logo',
+    width: 100,
+    height: 100,
+  },
+  navItems: [
+    {
+      label: 'Projects',
+      href: '/projects',
+    },
+  ] as NavItem[],
+};
